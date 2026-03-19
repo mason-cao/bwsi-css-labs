06e14fcda5a5ded2c8f2db273353716f933c5afd
Mason Cao <masoncao7@gmail.com>
2026-03-19 19:49:16 -0400

    Fix: add missing colon for list slicing

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -24,7 +24,7 @@ def max_subarray_sum(nums: list[int]) -> int:
         return 0
     max_current = max_global = nums[0]
 
-    for num in nums[1]:
+    for num in nums[1:]:
         max_current = max(num, max_current + num)
         if max_current > max_global:
             max_global = max_current
